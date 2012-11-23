3379b049b8285abcb3a12a8a0ef72619edf7ec95
Harm Behrens <hb@netzfisch.de>
2012-11-23 19:44:28 +0100

    removed dependent destroy

--- app/models/article.rb
+++ app/models/article.rb
@@ -22,8 +22,8 @@ class Article < Content
   has_many :categorizations
   has_many :categories, :through => :categorizations
   has_many :triggers, :as => :pending_item
-
-  has_many :comments,   :dependent => :destroy, :order => "created_at ASC" do
+# :dependent => :destroy,
+  has_many :comments,  :order => "created_at ASC" do
 
     # Get only ham or presumed_ham comments
     def ham
@@ -104,10 +104,10 @@ class Article < Content
     end
 
     def search_with_pagination(search_hash, paginate_hash)
-      
+
       state = (search_hash[:state] and ["no_draft", "drafts", "published", "withdrawn", "pending"].include? search_hash[:state]) ? search_hash[:state] : 'no_draft'
-      
-      
+
+
       list_function  = ["Article.#{state}"] + function_search_no_draft(search_hash)
 
       if search_hash[:category] and search_hash[:category].to_i > 0
@@ -467,3 +467,4 @@ class Article < Content
     return from..to
   end
 end
+
